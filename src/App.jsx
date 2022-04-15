04d01f3d23db1498abebb86b317d45ed8a76d004
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-15 09:51:05 +0200

    add components and add post functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { AddPostForm, PostList } from "./components";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Hello World", body: "Welcome to learning React!" },
+    { id: 2, title: "Installation", body: "You can install React from npm." },
+    {
+      id: 3,
+      title: "Up and running",
+      body: "You can start coding right away.",
+    },
+  ]);
+
+  const onAddPost = (post) => {
+    setPosts([...posts, { ...post, id: posts.length + 1 }]);
+  };
+
+  return (
+    <>
+      <AddPostForm onAddPost={onAddPost} />
+      <PostList posts={posts} title="ReactJS" />
+    </>
+  );
 }
 
 export default App;
