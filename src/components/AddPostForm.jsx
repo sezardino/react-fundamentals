1477c76610e5c00127a1723919980e616e1141bb
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-15 09:50:28 +0200

    add form for adding posts

---
+++ src/components/AddPostForm.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Button, Input } from "./ui";
+
+export const AddPostForm = (props) => {
+  const { onAddPost } = props;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const submitHandler = (evt) => {
+    evt.preventDefault();
+    onAddPost({ title, body });
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <form className="add-post-form" onSubmit={submitHandler}>
+      <Input
+        type="text"
+        name="title"
+        value={title}
+        placeholder="Title"
+        className="add-post-form__input"
+        onChange={(evt) => setTitle(evt.target.value)}
+      />
+      <Input
+        type="textarea"
+        name="body"
+        value={body}
+        placeholder="Body"
+        className="add-post-form__textarea"
+        onChange={(evt) => setBody(evt.target.value)}
+      />
+      <Button type="submit">Create new Post</Button>
+    </form>
+  );
+};
