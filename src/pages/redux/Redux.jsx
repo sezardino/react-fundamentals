876cafa2e86a5dc8f51112001c34230b30c5ffec
Edward Arechwa <e.arechwa@digitalcolliers.com>
2022-04-18 08:44:07 +0200

    add styles for customers page

--- src/pages/redux/Redux.jsx
+++ src/pages/redux/Redux.jsx
@@ -5,16 +5,18 @@ const Redux = () => {
   return (
     <div>
       <h1>Redux</h1>
-      <ul style={{ display: "flex", gap: 20 }}>
-        <li>
-          <Button to="bank">Bank</Button>
-        </li>
-        <li>
-          <Button to="customers">Customers</Button>
-        </li>
-      </ul>
-      <div>
-        <Outlet />
+      <div className="grid grid-cols-12 my-4">
+        <ul className="col-start-1 col-end-3 grid gap-6 justify-items-start">
+          <li>
+            <Button to="bank">Bank</Button>
+          </li>
+          <li>
+            <Button to="customers">Customers</Button>
+          </li>
+        </ul>
+        <div className="border-2 rounded-lg col-start-3 col-end-12 p-4">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
